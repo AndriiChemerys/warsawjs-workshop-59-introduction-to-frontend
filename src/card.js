5904ee637232c53e086507bd74518f1fd8e7b3c8
Andrii Chemerys <andriichemerys@gmail.com>
2021-11-21 14:16:59 +0100

    All cards viisble

---
+++ src/card.js
@@ -0,0 +1,11 @@
+import { cloneTemplateContent } from 'src/utils';    
+                                                                                        
+export class Card {                                                                   
+  constructor({ color, labelColor, template }) {                                      
+    this.color = color;                                                           
+    this.element = cloneTemplateContent(template);                                                  
+    this.element.textContent = color;                                                               
+    this.element.style.setProperty('--color', color);                                               
+    this.element.style.setProperty('--label-color', labelColor);                                    
+  }                                                                                             
+} 
--- src/index.js
+++ src/index.js
@@ -1,41 +1,26 @@
-import { colors } from 'src/data/colors';
-import { findElement , cloneTemplateContent} from 'src/utils';
-import 'src/index.scss';
-
-const boardTemplate = findElement(document, '#board-template');
-const slotTemplate = findElement(document, '#slot-template');
-const cardTemplate = findElement(document, '#card-template');
-
-function run() {
-  console.log('Memory Game');
-  const selectedColors = colors.slice(0,12);
-  console.log('colors', selectedColors);
-
-  // console.log('boardTemplate', boardTemplate);
-  // console.log('slotTemplate', slotTemplate);
-  // console.log('cardTemplate', cardTemplate);
-
-  const boardElement = cloneTemplateContent(boardTemplate);
-
-  // console.log('boardElement', boardElement);
-  // console.log('slotElement', slotElement);
-  // console.log('cardElement', cardElement);
-
-  document.body.prepend(boardElement);
-
-  selectedColors.forEach(({color, labelColor})=>{
-  const slotElement = cloneTemplateContent(slotTemplate);
-  const cardElement = cloneTemplateContent(cardTemplate);
-
-    // console.log('color', color);
-    // console.log('labelColor', labelColor);
-    cardElement.textContent=color;
-    cardElement.style.backgroundColor=color;
-    cardElement.style.color=labelColor;
-    boardElement.appendChild(slotElement);
-    slotElement.appendChild(cardElement);
-
-  })
-}
-
+import { colors } from 'src/data';      
+import { Card } from 'src/card';        
+import { Board } from 'src/board';      
+import { findElement } from 'src/utils';                                                                           
+import 'src/index.scss';                                                        
+                                                                                                      
+const boardTemplate = findElement(document, '#board-template');                                   
+const  slotTemplate = findElement(document, '#slot-template');                                    
+const  cardTemplate = findElement(document, '#card-template');                                    
+                                                                                              
+function run() {                        
+  console.log('Memory Game');           
+  const selectedColors = colors.slice(0, 12);    
+  console.log('selectedColors', selectedColors);    
+                                        
+  const board = new Board({ template: boardTemplate, slotTemplate });    
+                                        
+  selectedColors.forEach(({ color, labelColor }) => {    
+    const card = new Card({ color, labelColor, template: cardTemplate });    
+    board.addCard(card);                
+  });                                   
+                                        
+  document.body.prepend(board.element);           
+}                                                 
+                                            
 run();
